a2295c402fbce5aaaf13b5bc2352b46f84d2a5ea
smileluck <drenchedzlove@163.com>
2021-11-13 18:02:41 +0800

    first commit

---
+++ smile-system-boot/src/main/java/top/zsmile/SmileSystemApplication.java
@@ -0,0 +1,11 @@
+package top.zsmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmileSystemApplication.class, args);
+    }
+}
