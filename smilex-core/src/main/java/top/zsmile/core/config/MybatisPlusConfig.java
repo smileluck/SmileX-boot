5502f4e8d80e3bb14801fcc5e82cca2e34a6de4d
BetraySmile <drenchedzlove@163.com>
2022-03-11 17:52:04 +0800

    fix mybatisplusConfig 问题

---
+++ smilex-core/src/main/java/top/zsmile/core/config/MybatisPlusConfig.java
@@ -0,0 +1,9 @@
+package top.zsmile.core.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(value={"top.zsmile.modules.**.dao*"})
+public class MybatisPlusConfig {
+}
--- smilex-system-generator/src/main/java/top/zsmile/modules/generator/GeneratorApplication.java
+++ smilex-system-generator/src/main/java/top/zsmile/modules/generator/GeneratorApplication.java
@@ -1,11 +1,16 @@
 package top.zsmile.modules.generator;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"top.zsmile.*"})
+//@ComponentScan(basePackages = {"top.zsmile.core.*","top.zsmile.modules.generator.controller","top.zsmile.modules.generator.service"})
+//@MapperScan(basePackages = "top.zsmile.modules.generator.dao")
 public class GeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(GeneratorApplication.class, args);
