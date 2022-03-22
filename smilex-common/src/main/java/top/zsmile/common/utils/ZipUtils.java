1d95349a3f4ed22de4712e8413858a3a9e2fed02
smileluck <smilex@zsmile.top>
2022-03-22 17:57:41 +0800

    finish ZipUtils

--- smilex-common/src/main/java/top/zsmile/common/utils/ZipUtils.java
+++ smilex-common/src/main/java/top/zsmile/common/utils/ZipUtils.java
@@ -1,15 +1,29 @@
 package top.zsmile.common.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
+
 public class ZipUtils {
 
-    public static File createZip(String zipPath) {
+    private final static String FILE_SEPARATE = "/";
+
+    @Data
+    @AllArgsConstructor
+    public final static class ZipFileVO {
+        private String filePath;
+        private File file;
+    }
+
+    private static File createZip(String zipPath) {
         File zipFile = new File(zipPath);
         if (zipFile.exists()) {
             zipFile.delete();
@@ -22,12 +36,25 @@ public class ZipUtils {
         return zipFile;
     }
 
+    public static File createZip(String zipPath, String rootSrc, List outFiles) {
+        File zip = createZip(zipPath);
+        compress(zip, rootSrc, outFiles);
+        return zip;
+    }
 
-    public static void compress(File zipFile, String rootSrc, List<File> outFile) {
+
+    public static void compress(File zipFile, String rootSrc, List outFile) {
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFile));) {
-            for (File file : outFile) {
-                FileInputStream fileInputStream = new FileInputStream(file);
-                ZipEntry zipEntry = new ZipEntry(rootSrc + "/" + file.getName());
+            for (Object file : outFile) {
+                ZipEntry zipEntry = null;
+                FileInputStream fileInputStream;
+                if (file.getClass() == ZipFileVO.class) {
+                    fileInputStream = new FileInputStream(((ZipFileVO) file).getFile());
+                    zipEntry = new ZipEntry(rootSrc + FILE_SEPARATE + ((ZipFileVO) file).getFilePath() + FILE_SEPARATE + ((ZipFileVO) file).getFile().getName());
+                } else {
+                    fileInputStream = new FileInputStream((File) file);
+                    zipEntry = new ZipEntry(rootSrc + FILE_SEPARATE + ((File) file).getName());
+                }
                 zipOutputStream.putNextEntry(zipEntry);
                 int length;
                 byte[] buffer = new byte[1024];
@@ -54,7 +81,7 @@ public class ZipUtils {
             while ((length = fileInputStream.read(buffer)) > 0) {
                 zipOutputStream.write(buffer, 0, length);
             }
-            zipOutputStream.finish();
+            zipOutputStream.flush();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -64,29 +91,13 @@ public class ZipUtils {
 
     public static void main(String[] args) {
         File zipFile = createZip("D:\\test\\code.zip");
-        List<File> fileList = new ArrayList<>();
+        List fileList = new ArrayList<>();
         fileList.add(new File("D:\\test\\code\\java\\top\\zsmile\\modules\\common\\utils\\CmdUtils.java"));
         fileList.add(new File("D:\\test\\code\\java\\top\\zsmile\\modules\\common\\utils\\ConvertUtils.java"));
         fileList.add(new File("D:\\test\\code\\java\\top\\zsmile\\modules\\common\\utils\\MD5SignUtils.java"));
         fileList.add(new File("D:\\test\\code\\java\\top\\zsmile\\modules\\common\\utils\\MD5Utils.java"));
-        fileList.add(new File("D:\\test\\code\\java\\top\\zsmile\\modules\\common\\utils\\NameStyleUtils.java"));
-
+        fileList.add(new ZipFileVO("test", new File("D:\\test\\code\\java\\top\\zsmile\\modules\\common\\utils\\NameStyleUtils.java")));
         compress(zipFile, "/top/zsmile/modules", fileList);
-    }
-
-    public static void main1(String[] args) throws IOException {
-        File zipFile = new File("D:\\test\\code.zip");
-        FileOutputStream fileOutputStream = new FileOutputStream(zipFile);
-        ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
-        ZipEntry zipEntry = new ZipEntry("/top/zsmile/modules/测试.txt");
-//        zipEntry.
-        zipOutputStream.putNextEntry(zipEntry);
-//        zipOutputStream.finish();
-        zipOutputStream.flush();
-//        ZipEntry zipEntry2 = new ZipEntry("/top/zsmile/modules/测试2.txt");
-//        zipOutputStream.putNextEntry(zipEntry2);
-//        zipEntry.
-//        zipOutputStream.putNextEntry(zipEntry);
-//        zipOutputStream.finish();
+        
     }
 }
