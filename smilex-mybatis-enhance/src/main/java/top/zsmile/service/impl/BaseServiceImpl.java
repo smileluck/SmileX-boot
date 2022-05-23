53b0c5fcf755504d48c0fb1439f2f07508f2ed15
smileluck <smilex@zsmile.top>
2022-05-23 18:31:45 +0800

    update serviceimpl: variable name from baseDao to baseMapper

--- smilex-mybatis-enhance/src/main/java/top/zsmile/service/impl/BaseServiceImpl.java
+++ smilex-mybatis-enhance/src/main/java/top/zsmile/service/impl/BaseServiceImpl.java
@@ -4,24 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
 import top.zsmile.dao.BaseMapper;
 import top.zsmile.service.BaseService;
 
-import java.io.Serializable;
 import java.util.Collection;
-import java.util.Iterator;
 
 public class BaseServiceImpl<M extends BaseMapper<T>, T> implements BaseService<T> {
 
     protected Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    protected M baseDao;
+    protected M baseMapper;
 
     @Override
     public M getBaseMapper() {
-        return baseDao;
+        return baseMapper;
     }
 
     /**
