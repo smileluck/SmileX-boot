c2894116cdb5d29edaa2eac9977d75adb85aff31
smileluck <smilex@zsmile.top>
2022-05-26 18:28:57 +0800

    add MybatisConfig.java

---
+++ smilex-mybatis-enhance/src/main/java/top/zsmile/config/MybatisConfig.java
@@ -0,0 +1,54 @@
+package top.zsmile.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MybatisConfig {
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+//        factoryBean.setDataSource(dataSource);
+//        return factoryBean.getObject();
+//    }
+//
+//    @Bean
+//    public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) throws Exception {
+//        return new SqlSessionTemplate(sqlSessionFactory);
+//    }
+
+
+//    @Bean(name = "sqlSessionFactory")
+//    public SqlSessionFactory sqlSessionFactory(@Qualifier("dynamicDataSource") DataSource dynamicDataSource)
+//            throws Exception {
+//        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+//
+//        GlobalConfig globalConfig = new GlobalConfig();
+//        globalConfig.setBanner(false);
+//
+//        GlobalConfig.DbConfig dbConfig = new GlobalConfig.DbConfig();
+//        dbConfig.setIdType(IdType.ASSIGN_ID);
+//        dbConfig.setLogicDeleteField("del_flag");
+//        dbConfig.setLogicDeleteValue("1");
+//        dbConfig.setLogicNotDeleteValue("0");
+//        dbConfig.setTableUnderline(true);
+//        globalConfig.setDbConfig(dbConfig);
+//
+//
+//        MybatisConfiguration configuration = new MybatisConfiguration();
+//        configuration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+//        configuration.setCallSettersOnNulls(true);
+//
+//        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:/mapper/**/*.xml"));
+//        sessionFactory.setGlobalConfig(globalConfig);
+//        sessionFactory.setDataSource(dynamicDataSource);
+//        sessionFactory.setConfiguration(configuration);
+//        return sessionFactory.getObject();
+//    }
+}
