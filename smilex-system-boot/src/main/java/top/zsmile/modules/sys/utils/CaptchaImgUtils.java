84c87ed89fb1eb5ab4da2c39f9da5da7051749a1
smileluck <smilex@zsmile.top>
2022-06-24 18:38:59 +0800

    update

--- smilex-system-boot/src/main/java/top/zsmile/modules/sys/utils/CaptchaImgUtils.java
+++ smilex-system-boot/src/main/java/top/zsmile/modules/sys/utils/CaptchaImgUtils.java
@@ -1,5 +1,9 @@
 package top.zsmile.modules.sys.utils;
 
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.image.BufferedImage;
+
 /**
  * 验证码工具
  */
@@ -33,4 +37,26 @@ public class CaptchaImgUtils {
      * base64 图片前缀
      */
     private static final String BASE64_PRE = "data:image/jpg;base64,";
+
+    public static void generateCaptcha() {
+        //创建bufferImage对象
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        //获取Graphics2D 绘制对象
+        Graphics2D graphics = bufferedImage.createGraphics();
+        //设置文件
+        Font font = new Font("微软雅黑", Font.PLAIN, 20);
+        // 设置颜色
+        Color black = Color.black;
+
+        // 设置到绘制对象中
+        graphics.setColor(black);
+        graphics.setFont(font);
+        graphics.setBackground(Color.WHITE);
+
+        // 开始绘制
+        graphics.fillRect(0, 0, width, height);
+        // 获取上下文
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+
+    }
 }
