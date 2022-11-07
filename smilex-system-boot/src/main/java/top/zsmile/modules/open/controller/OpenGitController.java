df45e6a8013023a7dc7d4ebf687a73ae76f57e9a
smileluck <12386904+smileluck@users.noreply.github.com>
2022-11-07 15:49:34 +0800

    update webhook

--- smilex-system-boot/src/main/java/top/zsmile/modules/open/controller/OpenGitController.java
+++ smilex-system-boot/src/main/java/top/zsmile/modules/open/controller/OpenGitController.java
@@ -2,21 +2,36 @@ package top.zsmile.modules.open.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.zsmile.core.api.R;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
 @Api(tags = "开放Git webHook接口")
 @RestController
+@Slf4j
 @RequestMapping("/open/git")
 public class OpenGitController {
 
     @RequestMapping("/webhook")
-    public R webhook(@RequestBody JSONObject jsonObject) {
+    public String webhook(@RequestBody JSONObject jsonObject, HttpServletRequest httpServletRequest) {
 
-        return R.success();
+        Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+        log.info("headerNames==>{}", headerNames);
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String header = httpServletRequest.getHeader(name);
+            log.info("header {} ==> {}", name, header);
+        }
+        log.info("payload ==> {}", jsonObject);
+
+
+        return "SUCCESS";
     }
 
 }
