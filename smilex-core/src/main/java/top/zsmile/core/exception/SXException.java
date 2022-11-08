f9552745dca163e47e251ef2bd06d68d07ee539f
smileluck <12386904+smileluck@users.noreply.github.com>
2022-11-08 18:25:52 +0800

    feat: add final SXException

--- smilex-core/src/main/java/top/zsmile/core/exception/SXException.java
+++ smilex-core/src/main/java/top/zsmile/core/exception/SXException.java
@@ -1,6 +1,6 @@
 package top.zsmile.core.exception;
 
-public class SXException extends RuntimeException {
+public final class SXException extends RuntimeException {
 
     public SXException(String message) {
         super(message);
