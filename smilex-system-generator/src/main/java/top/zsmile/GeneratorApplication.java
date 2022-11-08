9f354d8281e903d081056556af6df967eb1b9e8a
smileluck <12386904+smileluck@users.noreply.github.com>
2022-11-08 18:28:19 +0800

    feat: update

--- smilex-system-generator/src/main/java/top/zsmile/GeneratorApplication.java
+++ smilex-system-generator/src/main/java/top/zsmile/GeneratorApplication.java
@@ -8,9 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.zsmile"})
-//@MapperScan(basePackages = {"top.zsmile.modules.generator.dao"})
-//@ComponentScans({@ComponentScan("com.zsmile")})
 public class GeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(GeneratorApplication.class, args);
