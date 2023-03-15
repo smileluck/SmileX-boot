5cc5481adfc312b00449cee631dc610268f45153
smileluck <12386904+smileluck@users.noreply.github.com>
2023-03-15 17:22:38 +0800

    docx transfer to html

--- smilex-study/doc/框架/ApachePoi/poi-解析docx文档.md
+++ smilex-study/doc/框架/ApachePoi/poi-解析docx文档.md
@@ -31,6 +31,7 @@
 - 段落水平对齐方式
 - 图片读取并转为base64
 - 字体类型，大小，高亮，颜色，加粗，下划线，斜体
+- 表格读取、背景颜色设置。
 
 注意事项：
 
@@ -45,74 +46,125 @@ public void poiTest() throws IOException {
     String filename = "D:/test/doc/import.docx";
     InputStream is = new FileInputStream(filename);
     XWPFDocument doc = new XWPFDocument(is);
-    List<XWPFParagraph> paragraphs = doc.getParagraphs();
+    List<IBodyElement> bodyElements = doc.getBodyElements();
+
     StringBuilder htmlStrBuild = new StringBuilder();
+    for (IBodyElement bodyElement : bodyElements) {
+        if (bodyElement.getElementType().equals(BodyElementType.PARAGRAPH)) {
+            XWPFParagraph paragraph = (XWPFParagraph) bodyElement;
 
-    for (XWPFParagraph para : paragraphs) {
-        // 段落属性
-        String paragraphStyle = "";
-        //对齐方式 alignment 枚举值
-        if (para.getAlignment().equals(ParagraphAlignment.CENTER)) {
-            paragraphStyle += "text-align:center;";
-        } else if (para.getAlignment().equals(ParagraphAlignment.LEFT)) {
-            paragraphStyle += "text-align:left;";
-        } else if (para.getAlignment().equals(ParagraphAlignment.RIGHT)) {
-            paragraphStyle += "text-align:right;";
-        }
-
-        //获取段落所有的文本对象
-        List<XWPFRun> runs = para.getRuns();
-        htmlStrBuild.append("<p style=\"" + paragraphStyle + "\">");
-        for (XWPFRun run : runs) {
-            // 图片处理
-            List<XWPFPicture> embeddedPictures = run.getEmbeddedPictures();
-            if (CollectionUtils.isNotEmpty(embeddedPictures)) {
-                for (XWPFPicture embeddedPicture : embeddedPictures) {
-                    htmlStrBuild.append("<p>");
-                    XWPFPictureData pictureData = embeddedPicture.getPictureData();
-                    String encode = "data:" + pictureData.getPackagePart().getContentType() + ";base64," + new String(Base64Utils.encode(pictureData.getData()));
-                    htmlStrBuild.append("<img src=\"" + encode + "\"/>");
-                    htmlStrBuild.append("</p>");
-                }
+            // 段落属性
+            String paragraphStyle = "";
+            //对齐方式 alignment 枚举值
+            if (paragraph.getAlignment().equals(ParagraphAlignment.CENTER)) {
+                paragraphStyle += "text-align:center;";
+            } else if (paragraph.getAlignment().equals(ParagraphAlignment.LEFT)) {
+                paragraphStyle += "text-align:left;";
+            } else if (paragraph.getAlignment().equals(ParagraphAlignment.RIGHT)) {
+                paragraphStyle += "text-align:right;";
             }
 
-            if (StringUtils.isNotBlank(run.text())) {
-                String style = "";
+            //获取段落所有的文本对象
+            List<XWPFRun> runs = paragraph.getRuns();
+            htmlStrBuild.append("<p style=\"" + paragraphStyle + "\">");
+            for (XWPFRun run : runs) {
+                // 图片处理
+                List<XWPFPicture> embeddedPictures = run.getEmbeddedPictures();
+                if (CollectionUtils.isNotEmpty(embeddedPictures)) {
+                    for (XWPFPicture embeddedPicture : embeddedPictures) {
+                        htmlStrBuild.append("<p>");
+                        XWPFPictureData pictureData = embeddedPicture.getPictureData();
+                        String encode = "data:" + pictureData.getPackagePart().getContentType() + ";base64," + new String(Base64Utils.encode(pictureData.getData()));
+                        htmlStrBuild.append("<img src=\"" + encode + "\"/>");
+                        htmlStrBuild.append("</p>");
+                    }
+                }
 
-                //文本的颜色 color
-                if (StringUtils.isNotBlank(run.getColor())) {
-                    style += "color:#" + run.getColor() + ";";
+                if (StringUtils.isNotBlank(run.text())) {
+                    String style = "";
+
+                    //文本的颜色 color
+                    if (StringUtils.isNotBlank(run.getColor())) {
+                        style += "color:#" + run.getColor() + ";";
+                    }
+                    //文本 大小 fontSize
+                    if (run.getFontSizeAsDouble() > 0) {
+                        style += "font-size:" + run.getFontSizeAsDouble().intValue() + "px;";
+                    }
+                    //文本 类型 fontFamily
+                    if (StringUtils.isNotBlank(run.getFontFamily())) {
+                        style += "font-family:" + run.getFontFamily() + ";";
+                    }
+                    //文本 加粗
+                    if (run.isBold()) {
+                        style += "font-weight: bolder;";
+                    }
+                    //文本 斜体
+                    if (run.isItalic()) {
+                        style += "font-style:italic;";
+                    }
+                    //文本 下划线
+                    if (run.getUnderline().equals(UnderlinePatterns.SINGLE)) {
+                        style += "text-decoration: underline;";
+                    }
+                    //文本 高亮
+                    if (run.isHighlighted()) {
+                        style += "background-color:" + run.getTextHighlightColor() + ";";
+                    }
+                    htmlStrBuild.append(String.format("<span style=\"%s\">%s</span>", style, run.text()));
                 }
-                //文本 大小 fontSize
-                if (run.getFontSizeAsDouble() > 0) {
-                    style += "font-size:" + run.getFontSizeAsDouble().intValue() + "px;";
-                }
-                //文本 类型 fontFamily
-                if (StringUtils.isNotBlank(run.getFontFamily())) {
-                    style += "font-family:" + run.getFontFamily() + ";";
-                }
-                //文本 加粗
-                if (run.isBold()) {
-                    style += "font-weight: bolder;";
-                }
-                //文本 斜体
-                if (run.isItalic()) {
-                    style += "font-style:italic;";
-                }
-                //文本 下划线
-                if (run.getUnderline().equals(UnderlinePatterns.SINGLE)) {
-                    style += "text-decoration: underline;";
-                }
-                //文本 高亮
-                if (run.isHighlighted()) {
-                    style += "background-color:" + run.getTextHighlightColor() + ";";
-                }
-                htmlStrBuild.append(String.format("<span style=\"%s\">%s</span>", style, run.text()));
             }
+            htmlStrBuild.append("</p>");
+        } else if (bodyElement.getElementType().equals(BodyElementType.TABLE)) {
+            XWPFTable table = (XWPFTable) bodyElement;
+            String tableStr = "<table border=\"1\">";
+            List<XWPFTableRow> rows = table.getRows();
+            for (XWPFTableRow row : rows) {
+                String rowStyle = "";
+                // 行高
+                if (row.getHeight() > 0) {
+                    rowStyle += "height:" + (row.getHeight()/10) + "px;";
+                }
+                tableStr += "<tr style=\"" + rowStyle + "\">";
+                List<XWPFTableCell> tableCells = row.getTableCells();
+                for (XWPFTableCell tableCell : tableCells) {
+                    String cellStyle = "";
+                    if (tableCell.getWidth() > 0) {
+                        cellStyle += "width:" + tableCell.getWidth() + "px;";
+                    }
+                    String cellFillHex = getCellFillHex(tableCell);
+                    if (cellFillHex != null) {
+                        cellStyle += "background-color:#" + cellFillHex + ';';
+                    }
+                    // 获取文本
+                    tableStr += "<td style=\"" + cellStyle + "\">";
+                    String text = tableCell.getText();
+                    tableStr += text;
+                    tableStr += "<td>";
+                }
+                tableStr += "</tr>";
+            }
+            tableStr += "</table>";
+            htmlStrBuild.append(tableStr);
         }
-        htmlStrBuild.append("</p>");
     }
+
     log.info(htmlStrBuild.toString());
+}
+
+
+private String getCellFillHex(XWPFTableCell tableCell) {
+    if (tableCell.getCTTc() != null) {
+        if (tableCell.getCTTc().getTcPr() != null && tableCell.getCTTc().getTcPr().getShd() != null) {
+            Object fill = tableCell.getCTTc().getTcPr().getShd().getFill();
+            if ("auto".equals(fill.toString())) {
+                return null;
+            } else if (fill instanceof byte[]) {
+                return CmdUtils.bytesToHexString((byte[]) fill);
+            }
+        }
+    }
+    return null;
 }
 ```
 
--- smilex-study/src/test/java/top/zsmile/test/poi/DocExtractTest.java
+++ smilex-study/src/test/java/top/zsmile/test/poi/DocExtractTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Base64Utils;
 import org.zwobble.mammoth.DocumentConverter;
 import org.zwobble.mammoth.Result;
+import top.zsmile.common.utils.CmdUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -54,93 +55,132 @@ public class DocExtractTest {
         String filename = "D:/test/doc/import.docx";
         InputStream is = new FileInputStream(filename);
         XWPFDocument doc = new XWPFDocument(is);
-        List<XWPFParagraph> paragraphs = doc.getParagraphs();
+        List<IBodyElement> bodyElements = doc.getBodyElements();
+
         StringBuilder htmlStrBuild = new StringBuilder();
-
-        for (XWPFParagraph para : paragraphs) {
-            // 段落属性
-            String paragraphStyle = "";
-            //对齐方式 alignment 枚举值
-            if (para.getAlignment().equals(ParagraphAlignment.CENTER)) {
-                paragraphStyle += "text-align:center;";
-            } else if (para.getAlignment().equals(ParagraphAlignment.LEFT)) {
-                paragraphStyle += "text-align:left;";
-            } else if (para.getAlignment().equals(ParagraphAlignment.RIGHT)) {
-                paragraphStyle += "text-align:right;";
-            }
-
-            //获取段落所有的文本对象
-            List<XWPFRun> runs = para.getRuns();
-            htmlStrBuild.append("<p style=\"" + paragraphStyle + "\">");
-            for (XWPFRun run : runs) {
-                // 图片处理
-                List<XWPFPicture> embeddedPictures = run.getEmbeddedPictures();
-                if (CollectionUtils.isNotEmpty(embeddedPictures)) {
-                    for (XWPFPicture embeddedPicture : embeddedPictures) {
-                        htmlStrBuild.append("<p>");
-                        XWPFPictureData pictureData = embeddedPicture.getPictureData();
-                        String encode = "data:" + pictureData.getPackagePart().getContentType() + ";base64," + new String(Base64Utils.encode(pictureData.getData()));
-                        htmlStrBuild.append("<img src=\"" + encode + "\"/>");
-                        htmlStrBuild.append("</p>");
+        for (IBodyElement bodyElement : bodyElements) {
+            if (bodyElement.getElementType().equals(BodyElementType.PARAGRAPH)) {
+                XWPFParagraph paragraph = (XWPFParagraph) bodyElement;
+                htmlStrBuild.append(paragraphHandle(paragraph));
+            } else if (bodyElement.getElementType().equals(BodyElementType.TABLE)) {
+                XWPFTable table = (XWPFTable) bodyElement;
+                String tableStr = "<table cellpadding=\"0\" border=\"1\" cellspacing=\"0\">";
+                List<XWPFTableRow> rows = table.getRows();
+                for (XWPFTableRow row : rows) {
+                    String rowStyle = "";
+                    // 行高
+                    if (row.getHeight() > 0) {
+                        rowStyle += "height:" + (row.getHeight() / 10) + "px;";
                     }
+                    tableStr += "<tr style=\"" + rowStyle + "\">";
+                    List<XWPFTableCell> tableCells = row.getTableCells();
+                    for (XWPFTableCell tableCell : tableCells) {
+                        String cellStyle = "";
+                        if (tableCell.getWidth() > 0) {
+                            cellStyle += "width:" + tableCell.getWidth() + "px;";
+                        }
+                        String cellFillHex = getCellFillHex(tableCell);
+                        if (cellFillHex != null) {
+                            cellStyle += "background-color:#" + cellFillHex + ';';
+                        }
+                        // 获取文本
+                        tableStr += "<td style=\"" + cellStyle + "\">";
+
+                        List<XWPFParagraph> paragraphs = tableCell.getParagraphs();
+                        for (XWPFParagraph paragraph : paragraphs) {
+                            tableStr += paragraphHandle(paragraph);
+                        }
+                        tableStr += "<td>";
+                    }
+                    tableStr += "</tr>";
                 }
-
-                if (StringUtils.isNotBlank(run.text())) {
-                    String style = "";
-
-                    //文本的颜色 color
-                    if (StringUtils.isNotBlank(run.getColor())) {
-                        style += "color:#" + run.getColor() + ";";
-                    }
-                    //文本 大小 fontSize
-                    if (run.getFontSizeAsDouble() > 0) {
-                        style += "font-size:" + run.getFontSizeAsDouble().intValue() + "px;";
-                    }
-                    //文本 类型 fontFamily
-                    if (StringUtils.isNotBlank(run.getFontFamily())) {
-                        style += "font-family:" + run.getFontFamily() + ";";
-                    }
-                    //文本 加粗
-                    if (run.isBold()) {
-                        style += "font-weight: bolder;";
-                    }
-                    //文本 斜体
-                    if (run.isItalic()) {
-                        style += "font-style:italic;";
-                    }
-                    //文本 下划线
-                    if (run.getUnderline().equals(UnderlinePatterns.SINGLE)) {
-                        style += "text-decoration: underline;";
-                    }
-                    //文本 高亮
-                    if (run.isHighlighted()) {
-                        style += "background-color:" + run.getTextHighlightColor() + ";";
-                    }
-                    htmlStrBuild.append(String.format("<span style=\"%s\">%s</span>", style, run.text()));
-                }
+                tableStr += "</table>";
+                htmlStrBuild.append(tableStr);
             }
-            // 获取段落所有的表格
-//            List<XWPFTable> tables = para.getBody().getTables();
-//            for (XWPFTable table : tables) {
-//                String tableStr = "<table>";
-//                List<XWPFTableRow> rows = table.getRows();
-//                for (XWPFTableRow row : rows) {
-//                    tableStr += "<tr>";
-//                    List<XWPFTableCell> tableCells = row.getTableCells();
-//                    for (XWPFTableCell tableCell : tableCells) {
-//                        // 获取文本
-//                        tableStr += "<td>";
-//                        String text = tableCell.getText();
-//                        tableStr += text;
-//                        tableStr += "<td>";
-//                    }
-//                    tableStr += "</tr>";
-//                }
-//                tableStr += "</table>";
-//                htmlStrBuild.append(tableStr);
-//            }
-            htmlStrBuild.append("</p>");
         }
+
         log.info(htmlStrBuild.toString());
+    }
+
+
+    private String getCellFillHex(XWPFTableCell tableCell) {
+        if (tableCell.getCTTc() != null) {
+            if (tableCell.getCTTc().getTcPr() != null && tableCell.getCTTc().getTcPr().getShd() != null) {
+                Object fill = tableCell.getCTTc().getTcPr().getShd().getFill();
+                if ("auto".equals(fill.toString())) {
+                    return null;
+                } else if (fill instanceof byte[]) {
+                    return CmdUtils.bytesToHexString((byte[]) fill);
+                }
+            }
+        }
+        return null;
+    }
+
+    private String paragraphHandle(XWPFParagraph paragraph) {
+        StringBuilder htmlStrBuild = new StringBuilder();
+        // 段落属性
+        String paragraphStyle = "";
+        //对齐方式 alignment 枚举值
+        if (paragraph.getAlignment().equals(ParagraphAlignment.CENTER)) {
+            paragraphStyle += "text-align:center;";
+        } else if (paragraph.getAlignment().equals(ParagraphAlignment.LEFT)) {
+            paragraphStyle += "text-align:left;";
+        } else if (paragraph.getAlignment().equals(ParagraphAlignment.RIGHT)) {
+            paragraphStyle += "text-align:right;";
+        }
+
+        //获取段落所有的文本对象
+        List<XWPFRun> runs = paragraph.getRuns();
+        htmlStrBuild.append("<p style=\"" + paragraphStyle + "\">");
+        for (XWPFRun run : runs) {
+            // 图片处理
+            List<XWPFPicture> embeddedPictures = run.getEmbeddedPictures();
+            if (CollectionUtils.isNotEmpty(embeddedPictures)) {
+                for (XWPFPicture embeddedPicture : embeddedPictures) {
+                    htmlStrBuild.append("<p>");
+                    XWPFPictureData pictureData = embeddedPicture.getPictureData();
+                    String encode = "data:" + pictureData.getPackagePart().getContentType() + ";base64," + new String(Base64Utils.encode(pictureData.getData()));
+                    htmlStrBuild.append("<img src=\"" + encode + "\"/>");
+                    htmlStrBuild.append("</p>");
+                }
+            }
+
+            if (StringUtils.isNotBlank(run.text())) {
+                String style = "";
+
+                //文本的颜色 color
+                if (StringUtils.isNotBlank(run.getColor())) {
+                    style += "color:#" + run.getColor() + ";";
+                }
+                //文本 大小 fontSize
+                if (run.getFontSizeAsDouble() != null && run.getFontSizeAsDouble() > 0) {
+                    style += "font-size:" + run.getFontSizeAsDouble().intValue() + "px;";
+                }
+                //文本 类型 fontFamily
+                if (StringUtils.isNotBlank(run.getFontFamily())) {
+                    style += "font-family:" + run.getFontFamily() + ";";
+                }
+                //文本 加粗
+                if (run.isBold()) {
+                    style += "font-weight: bolder;";
+                }
+                //文本 斜体
+                if (run.isItalic()) {
+                    style += "font-style:italic;";
+                }
+                //文本 下划线
+                if (run.getUnderline().equals(UnderlinePatterns.SINGLE)) {
+                    style += "text-decoration: underline;";
+                }
+                //文本 高亮
+                if (run.isHighlighted()) {
+                    style += "background-color:" + run.getTextHighlightColor() + ";";
+                }
+                htmlStrBuild.append(String.format("<span style=\"%s\">%s</span>", style, run.text()));
+            }
+        }
+        htmlStrBuild.append("</p>");
+        return htmlStrBuild.toString();
     }
 }
