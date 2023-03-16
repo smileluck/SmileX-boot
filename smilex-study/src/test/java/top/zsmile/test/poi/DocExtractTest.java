11096fee4ddd8558bba0427077696b52628a2b2f
smileluck <12386904+smileluck@users.noreply.github.com>
2023-03-16 17:30:14 +0800

    docx transfer to html

--- smilex-study/src/test/java/top/zsmile/test/poi/DocExtractTest.java
+++ smilex-study/src/test/java/top/zsmile/test/poi/DocExtractTest.java
@@ -5,6 +5,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.xwpf.usermodel.*;
 import org.junit.Test;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTShd;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Base64Utils;
 import org.zwobble.mammoth.DocumentConverter;
@@ -79,9 +80,56 @@ public class DocExtractTest {
                         if (tableCell.getWidth() > 0) {
                             cellStyle += "width:" + tableCell.getWidth() + "px;";
                         }
-                        String cellFillHex = getCellFillHex(tableCell);
-                        if (cellFillHex != null) {
-                            cellStyle += "background-color:#" + cellFillHex + ';';
+                        // 垂直剧中
+                        XWPFTableCell.XWPFVertAlign verticalAlignment = tableCell.getVerticalAlignment();
+                        if (verticalAlignment != null) {
+                            if (verticalAlignment.equals(XWPFTableCell.XWPFVertAlign.TOP)) {
+                                cellStyle += "vertical-align:top;";
+                            } else if (verticalAlignment.equals(XWPFTableCell.XWPFVertAlign.CENTER)) {
+                                cellStyle += "vertical-align:center;";
+                            } else if (verticalAlignment.equals(XWPFTableCell.XWPFVertAlign.BOTTOM)) {
+                                cellStyle += "vertical-align:bottom;";
+                            }
+                        }
+
+                        // pct-50
+                        CTShd cellShd = getCellShd(tableCell);
+                        if (cellShd != null) {
+                            // 设置单元格背景色
+                            String cellFillHex = getCellFillHex(tableCell);
+                            if (cellFillHex != null) {
+                                cellStyle += "background-color:#" + cellFillHex + ';';
+                            }
+
+                            // 设置单元格底纹
+                            String cellColorHex = getCellColorHex(cellShd);
+                            if (StringUtils.isBlank(cellColorHex)) {
+                                cellColorHex = "000";
+                            }
+                            String shdValue = cellShd.getVal().toString();
+                            if (shdValue.equals("solid")) {
+                                cellStyle += String.format("background-color:#%s;", cellColorHex);
+                            } else if (shdValue.startsWith("pct")) {
+                                cellStyle += String.format("background:radial-gradient(circle, #%s 0.5px, transparent 0.5px);", cellColorHex);
+                                Double num = Math.ceil(Double.valueOf(cellShd.getVal().toString().substring(3)) / 10);
+                                cellStyle += String.format("background-size: %spx %spx;", num, num);
+                            } else if (shdValue.equals("horzStripe")) {
+                                cellStyle += String.format("background-image: -webkit-linear-gradient(#%s 50%%, transparent 50%%, transparent);", cellColorHex);
+                                cellStyle += String.format("background-image: linear-gradient(#%s 50%%, transparent 50%%, transparent);", cellColorHex);
+                                cellStyle += String.format("background-size: 3px 3px;");
+                            } else if (shdValue.equals("vertStripe")) {
+                                cellStyle += String.format("background-image: -webkit-linear-gradient(to right,#%s 50%%, transparent 50%%, transparent);", cellColorHex);
+                                cellStyle += String.format("background-image: linear-gradient(to right,#%s 50%%, transparent 50%%, transparent);", cellColorHex);
+                                cellStyle += String.format("background-size: 3px 3px;");
+                            } else if (shdValue.equals("diagStripe")) {
+                                cellStyle += String.format("background-image: -webkit-linear-gradient(-45deg,#%s 25%%, transparent 25%%,transparent 50%%,#%s 50%%,#%s 75%%,transparent 75%%, transparent);", cellColorHex, cellColorHex,cellColorHex);
+                                cellStyle += String.format("background-image: linear-gradient(-45deg,#%s 25%%, transparent 25%%,transparent 50%%,#%s 50%%,#%s 75%%,transparent 75%%, transparent);", cellColorHex, cellColorHex,cellColorHex);
+                                cellStyle += String.format("background-size: 5px 5px;");
+                            } else if (shdValue.equals("reverseDiagStripe")) {
+                                cellStyle += String.format("background-image: -webkit-linear-gradient(45deg,#%s 25%%, transparent 25%%,transparent 50%%,#%s 50%%,#%s 75%%,transparent 75%%, transparent);", cellColorHex, cellColorHex,cellColorHex);
+                                cellStyle += String.format("background-image: linear-gradient(45deg,#%s 25%%, transparent 25%%,transparent 50%%,#%s 50%%,#%s 75%%,transparent 75%%, transparent);", cellColorHex, cellColorHex,cellColorHex);
+                                cellStyle += String.format("background-size: 5px 5px;");
+                            }
                         }
                         // 获取文本
                         tableStr += "<td style=\"" + cellStyle + "\">";
@@ -90,7 +138,7 @@ public class DocExtractTest {
                         for (XWPFParagraph paragraph : paragraphs) {
                             tableStr += paragraphHandle(paragraph);
                         }
-                        tableStr += "<td>";
+                        tableStr += "</td>";
                     }
                     tableStr += "</tr>";
                 }
@@ -103,19 +151,59 @@ public class DocExtractTest {
     }
 
 
-    private String getCellFillHex(XWPFTableCell tableCell) {
+    private CTShd getCellShd(XWPFTableCell tableCell) {
         if (tableCell.getCTTc() != null) {
-            if (tableCell.getCTTc().getTcPr() != null && tableCell.getCTTc().getTcPr().getShd() != null) {
-                Object fill = tableCell.getCTTc().getTcPr().getShd().getFill();
-                if ("auto".equals(fill.toString())) {
-                    return null;
-                } else if (fill instanceof byte[]) {
-                    return CmdUtils.bytesToHexString((byte[]) fill);
-                }
+            if (tableCell.getCTTc().getTcPr() != null) {
+                return tableCell.getCTTc().getTcPr().getShd();
             }
         }
         return null;
     }
+
+    private String getCellFillHex(XWPFTableCell tableCell) {
+        if (tableCell.getCTTc() != null) {
+            if (tableCell.getCTTc().getTcPr() != null && getCellShd(tableCell) != null) {
+                return getCellFillHex(getCellShd(tableCell));
+            }
+        }
+        return null;
+    }
+
+
+    private String getCellFillHex(CTShd chd) {
+        if (chd != null) {
+            Object fill = chd.getFill();
+            if ("auto".equals(fill.toString())) {
+                return null;
+            } else if (fill instanceof byte[]) {
+                return CmdUtils.bytesToHexString((byte[]) fill);
+            }
+        }
+        return null;
+    }
+
+
+    private String getCellColorHex(XWPFTableCell tableCell) {
+        if (tableCell.getCTTc() != null) {
+            if (tableCell.getCTTc().getTcPr() != null && getCellShd(tableCell) != null) {
+                return getCellColorHex(getCellShd(tableCell));
+            }
+        }
+        return null;
+    }
+
+    private String getCellColorHex(CTShd chd) {
+        if (chd != null) {
+            Object color = chd.getColor();
+            if ("auto".equals(color.toString())) {
+                return null;
+            } else if (color instanceof byte[]) {
+                return CmdUtils.bytesToHexString((byte[]) color);
+            }
+        }
+        return null;
+    }
+
 
     private String paragraphHandle(XWPFParagraph paragraph) {
         StringBuilder htmlStrBuild = new StringBuilder();
