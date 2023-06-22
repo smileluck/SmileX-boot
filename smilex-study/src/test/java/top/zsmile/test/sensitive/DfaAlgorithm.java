3b26668bedb26779c06de6683493f47bc78663d4
smileluck <12386904+smileluck@users.noreply.github.com>
2023-06-22 10:57:41 +0800

    update dfa

--- smilex-study/src/test/java/top/zsmile/test/sensitive/DfaAlgorithm.java
+++ smilex-study/src/test/java/top/zsmile/test/sensitive/DfaAlgorithm.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import top.zsmile.common.core.exception.SXException;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -119,12 +121,28 @@ public class DfaAlgorithm {
         }
     }
 
-    public void check(String text) {
+    /**
+     * 判断是否有敏感词
+     *
+     * @param text
+     * @return
+     */
+    public Boolean check(String text) {
+        DfaSearchResult first = findFirst(text);
+        return first != null;
+    }
+
+    /**
+     * 获取第一个
+     *
+     * @param text
+     * @return
+     */
+    public DfaSearchResult findFirst(String text) {
         char[] chars = text.toCharArray();
         StringBuilder sb = new StringBuilder();
         TrieNode trieNode = null;
         for (int i = 0; i < chars.length; i++) {
-
             for (int j = i; j < chars.length; j++) {
                 char aChar = chars[j];
                 if (trieNode == null) {
@@ -132,7 +150,51 @@ public class DfaAlgorithm {
                     if (trieNode != null) {
                         sb.append(aChar);
                         if (trieNode.getIsWord() == 1) {
-                            System.out.println(sb.toString());
+                            return new DfaSearchResult(i, sb.toString());
+                        }
+                    } else {
+                        break;
+                    }
+                } else {
+                    TrieNode trieNodeNext = trieNode.get(aChar);
+                    if (trieNodeNext == null) {
+                        sb.setLength(0);
+                        trieNode = null;
+                        break;
+                    } else {
+                        sb.append(aChar);
+                        trieNode = trieNodeNext;
+                        if (trieNodeNext.getIsWord() == 1) {
+                            return new DfaSearchResult(i, sb.toString());
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    /**
+     * 获取全部
+     *
+     * @param text
+     * @return
+     */
+    public List<DfaSearchResult> findAll(String text) {
+        char[] chars = text.toCharArray();
+        List<DfaSearchResult> results = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        TrieNode trieNode = null;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = i; j < chars.length; j++) {
+                char aChar = chars[j];
+                if (trieNode == null) {
+                    trieNode = map.get(aChar);
+                    if (trieNode != null) {
+                        sb.append(aChar);
+                        if (trieNode.getIsWord() == 1) {
+                            results.add(new DfaSearchResult(i, sb.toString()));
                             sb.setLength(0);
                             trieNode = null;
                             break;
@@ -150,7 +212,7 @@ public class DfaAlgorithm {
                         sb.append(aChar);
                         trieNode = trieNodeNext;
                         if (trieNodeNext.getIsWord() == 1) {
-                            System.out.println(sb.toString());
+                            results.add(new DfaSearchResult(i, sb.toString()));
                             sb.setLength(0);
                             trieNode = null;
                             i = j;
@@ -160,6 +222,66 @@ public class DfaAlgorithm {
                 }
             }
         }
+        return results;
     }
+
+
+    /**
+     * 替换掉敏感词
+     *
+     * @param text
+     * @return
+     */
+    public String replace(String text, Character replaceChar) {
+        char[] chars = text.toCharArray();
+        TrieNode trieNode = null;
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = i; j < chars.length; j++) {
+                char aChar = chars[j];
+                if (trieNode == null) {
+                    trieNode = map.get(aChar);
+                    if (trieNode != null) {
+                        if (trieNode.getIsWord() == 1) {
+                            replace(chars, replaceChar, i, j);
+                            trieNode = null;
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                } else {
+                    TrieNode trieNodeNext = trieNode.get(aChar);
+                    if (trieNodeNext == null) {
+                        trieNode = null;
+                        break;
+                    } else {
+                        trieNode = trieNodeNext;
+                        if (trieNodeNext.getIsWord() == 1) {
+                            replace(chars, replaceChar, i, j);
+                            trieNode = null;
+                            i = j;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    /**
+     * 替换字符数组中，下标范围的数据
+     *
+     * @param chars
+     * @param replaceChar
+     * @param start
+     * @param end
+     */
+    private void replace(char[] chars, char replaceChar, int start, int end) {
+        for (int i = start; i <= end; i++) {
+            chars[i] = replaceChar;
+        }
+    }
+
 
 }
--- smilex-study/src/test/java/top/zsmile/test/sensitive/DfaAlgorithmTest.java
+++ smilex-study/src/test/java/top/zsmile/test/sensitive/DfaAlgorithmTest.java
@@ -6,7 +6,11 @@ import org.springframework.core.io.ClassPathResource;
 import top.zsmile.common.core.utils.file.SpringFileUtils;
 
 import java.io.IOException;
+import java.util.List;
 
+/**
+ * 敏感词dfa测试单元
+ */
 public class DfaAlgorithmTest {
 
     @Test
@@ -14,6 +18,13 @@ public class DfaAlgorithmTest {
         DfaAlgorithm instance = DfaAlgorithm.getInstance();
         ClassPathResource classPathResource = new ClassPathResource("sensitive/illegal.txt");
         instance.readFile(classPathResource.getFile());
-        instance.check("军火炸药撒发射点爆破和杀伤燃烧弹发去委任他为");
+//        instance.check("军火炸药撒发射点爆破和杀伤燃烧弹发去委任他为");
+        long startTime = System.currentTimeMillis();
+        List<DfaSearchResult> results = instance.findAll("军火炸药撒发射点爆破和杀伤燃烧弹发去委任他为");
+
+        String newStr = instance.replace("军火炸药撒发射点爆破和杀伤燃烧弹发去委任他为", '*');
+        long endTime = System.currentTimeMillis();
+        System.out.println("spend time : " + (endTime - startTime));
+
     }
 }
