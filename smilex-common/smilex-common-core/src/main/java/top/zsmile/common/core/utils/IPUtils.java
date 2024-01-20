c35b9e753385a35b8c1ba9b531422ce182d964b5
smileluck <12386904+smileluck@users.noreply.github.com>
2024-01-20 21:18:28 +0800

    update IPUtils

--- smilex-common/smilex-common-core/src/main/java/top/zsmile/common/core/utils/IPUtils.java
+++ smilex-common/smilex-common-core/src/main/java/top/zsmile/common/core/utils/IPUtils.java
@@ -1,8 +1,21 @@
 package top.zsmile.common.core.utils;
 
-import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cglib.core.Local;
 
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * IP工具
+ */
 public class IPUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(IPUtils.class);
+    private static final String LOCAL_IP = "127.0.0.1";
+
     /**
      * 根据servlet请求获取ip地址
      *
@@ -19,6 +32,24 @@ public class IPUtils {
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = httpServletRequest.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    /**
+     * 获取服务器IP地址
+     *
+     * @return 服务器IP地址
+     */
+    public static String getServerIp() {
+        String ip;
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            ip = localHost.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            ip = LOCAL_IP;
+            logger.error("IPUtil getServerIp", e);
         }
         return ip;
     }
