c6f82368aadd2f6abfdd720aa87bcca43035f608
smileluck <12386904+smileluck@users.noreply.github.com>
2024-02-19 21:58:18 +0800

    feat: rpc

---
+++ smilex-modules/smilex-modules-rpc/src/main/java/top/zsmile/modules/rpc/dto/RpcRequest.java
@@ -0,0 +1,7 @@
+package top.zsmile.modules.rpc.dto;
+
+/**
+ * RPC 请求体
+ */
+public class RpcRequest {
+}
---
+++ smilex-modules/smilex-modules-rpc/src/main/java/top/zsmile/modules/rpc/dto/RpcResponse.java
@@ -0,0 +1,7 @@
+package top.zsmile.modules.rpc.dto;
+
+/**
+ * Rpc 响应体
+ */
+public class RpcResponse {
+}
