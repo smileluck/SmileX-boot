cb5f3a47e691ef37b7cb5d96490cb1cc39ba3488
smileluck <12386904+smileluck@users.noreply.github.com>
2024-03-31 16:48:33 +0800

    feat: update pom

--- smilex-tool/smilex-tool-gen/src/main/java/top/zsmile/tool/gen/constant/DefaultConstants.java
+++ smilex-tool/smilex-tool-gen/src/main/java/top/zsmile/tool/gen/constant/DefaultConstants.java
@@ -1,8 +1,9 @@
 package top.zsmile.tool.gen.constant;
 
-import com.google.common.collect.ImmutableList;
-
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,7 +17,7 @@ public class DefaultConstants implements Serializable {
     public static final String DATE_FORMAT = "yyyy-MM-dd";
     public static final String TIME_FORMAT = "HH:mm:ss";
     public static final String MYSQL_DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
-    public static final List<String> IGNORE_COLUMN = ImmutableList.of("create_time", "update_time", "create_by", "update_by");
+    public static final List<String> IGNORE_COLUMN = Collections.unmodifiableList(Arrays.asList("create_time", "update_time", "create_by", "update_by"));
     /**
      * 删除键名，1删除，0未删除
      */
