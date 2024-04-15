74f4b86d537a17183fb858964888c387cc720bce
smileluck <12386904+smileluck@users.noreply.github.com>
2024-04-15 14:58:52 +0800

    feat: add utils

---
+++ smilex-tool/smilex-tool-pay/src/main/java/top/zsmile/pay/utils/OrderUtils.java
@@ -0,0 +1,17 @@
+package top.zsmile.pay.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import top.zsmile.common.core.utils.SnowFlake;
+
+public final class OrderUtils {
+
+    private static final SnowFlake snowFlake = new SnowFlake(1, 1);
+
+    public static String getOrderId(String prefix) {
+        return StringUtils.isNotBlank(prefix) ? prefix + snowFlake.nextId() : snowFlake.nextId() + "";
+    }
+
+    public static String getOrderId() {
+        return getOrderId(null);
+    }
+}
