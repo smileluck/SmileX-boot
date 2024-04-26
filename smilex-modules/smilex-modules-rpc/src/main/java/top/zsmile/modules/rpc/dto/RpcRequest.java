4e3a327a3c1129b260a81389dd4037df71053e3e
smileluck <12386904+smileluck@users.noreply.github.com>
2024-04-26 11:00:17 +0800

    feat: temp add rpc service

--- smilex-modules/smilex-modules-rpc/src/main/java/top/zsmile/modules/rpc/dto/RpcRequest.java
+++ smilex-modules/smilex-modules-rpc/src/main/java/top/zsmile/modules/rpc/dto/RpcRequest.java
@@ -6,4 +6,6 @@ import java.io.Serializable;
  * RPC 请求体
  */
 public class RpcRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
 }
--- smilex-modules/smilex-modules-rpc/src/main/java/top/zsmile/modules/rpc/dto/RpcResponse.java
+++ smilex-modules/smilex-modules-rpc/src/main/java/top/zsmile/modules/rpc/dto/RpcResponse.java
@@ -1,7 +1,12 @@
 package top.zsmile.modules.rpc.dto;
 
+import java.io.Serializable;
+
 /**
  * Rpc 响应体
  */
-public class RpcResponse {
+public class RpcResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
 }
