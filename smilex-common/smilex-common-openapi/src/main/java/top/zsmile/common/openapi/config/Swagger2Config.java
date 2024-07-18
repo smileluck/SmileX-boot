cdf590d91814435193fddc7e519a6b6b8eb22329
smileluck <12386904+smileluck@users.noreply.github.com>
2024-07-18 10:47:29 +0800

    project: update framework pom

---
+++ smilex-common/smilex-common-openapi/src/main/java/top/zsmile/common/openapi/config/Swagger2Config.java
@@ -0,0 +1,14 @@
+package top.zsmile.common.openapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import top.zsmile.common.core.constant.CommonConstant;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class Swagger2Config {
+
+}
