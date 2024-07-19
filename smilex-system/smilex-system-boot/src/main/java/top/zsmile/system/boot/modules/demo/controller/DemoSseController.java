822542a6a902c82dfed610aaef4e38a3aa47f7e1
smileluck <12386904+smileluck@users.noreply.github.com>
2024-07-19 10:33:29 +0800

    feat: update swagger to openapi

--- smilex-system/smilex-system-boot/src/main/java/top/zsmile/system/boot/modules/demo/controller/DemoSseController.java
+++ smilex-system/smilex-system-boot/src/main/java/top/zsmile/system/boot/modules/demo/controller/DemoSseController.java
@@ -1,5 +1,7 @@
 package top.zsmile.system.boot.modules.demo.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,7 @@ import javax.annotation.Resource;
 /**
  * SSE测试
  */
+@Tag(name = "DEMO-SSE测试")
 @RestController
 @RequestMapping("/demo/sse")
 public class DemoSseController {
@@ -19,12 +22,14 @@ public class DemoSseController {
     @Resource
     private SseService sseService;
 
+    @Schema(description = "订阅")
     @RequestMapping(value = "/subscribe", produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
     public SseEmitter subscribe(String clientId) {
         return sseService.connect(clientId);
     }
 
 
+    @Schema(description = "发送")
     @RequestMapping(value = "/send")
     public R send(String clientId, String content) {
         if (sseService.send(clientId, content)) {
@@ -34,6 +39,7 @@ public class DemoSseController {
     }
 
 
+    @Schema(description = "关闭")
     @RequestMapping(value = "/close")
     public R close(String clientId) {
         sseService.close(clientId);
